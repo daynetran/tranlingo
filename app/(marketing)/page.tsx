dce54a460cb954d2003b2424907e99a792c769ca
Dayne Tran <daynetran256@gmail.com>
2024-03-30 12:13:01 -0700

    added layout, buttons, and media files

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <Button size="lg" variant="primary">Click me</Button>
+        </div>
+    )
+}
