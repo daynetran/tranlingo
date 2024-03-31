8e2cd8cc087f7d3911df6bdd487d9a5b0a9db59f
Dayne Tran <daynetran256@gmail.com>
2024-03-30 19:41:02 -0700

    added user progress

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,8 +1,9 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import { List } from "./list";
 
 const CoursesPage = async () => {
     const courses = await getCourses();
+    const userProgress = await getUserProgress();
 
     return (
         <div className="h-full max-w-[912px] px-3 mx-auto">
@@ -11,7 +12,7 @@ const CoursesPage = async () => {
             </h1>
             <List
                 courses={courses}
-                activeCourseId={1}
+                activeCourseId={userProgress?.activeCourseId}
             />
         </div>
     )
